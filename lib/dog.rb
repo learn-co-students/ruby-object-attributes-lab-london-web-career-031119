100ca88e5382a859a0d0aa94caa7630e135a06f3
github username <github email address>
2019-03-12 18:32:57 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+class Dog
+    def name(name_input)
+      @name = name_input
+    end
+
+    def name
+      @name
+    end
+
+    def name=(new_name_input)
+      @name = new_name_input
+    end
+
+    def breed(breed_input)
+      @breed = breed_input
+    end
+
+    def breed
+      @breed
+    end
+
+    def breed=(new_breed)
+      @breed = new_breed
+    end
+end
